99425c105e2336f29d0ad829996a26dbca947248
juiceman1014 <justintran1014@gmail.com>
2025-10-16 22:45:33 -0700

    implemented multiplication code to test autograder with

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-    print("Hello from main.py!")
+    user_input = input()
+    try:
+        num = int(user_input)
+        print(num * num)
+    except Exception as e:
+        print(f"error: {e}")
 
 if __name__ == "__main__":
     main()
